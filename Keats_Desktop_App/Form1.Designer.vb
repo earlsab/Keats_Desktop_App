40222f185783506eded01a7756c72ac1593a5123
earlsab <62688314+earlsab@users.noreply.github.com>
2023-05-17 14:30:00 +0800

    Added Login Form & Main Form

--- Keats_Desktop_App/Form1.Designer.vb
+++ Keats_Desktop_App/Form1.Designer.vb
@@ -22,17 +22,30 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(150, 134)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 0
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(581, 438)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Test Form"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
 
 End Class
--- Keats_Desktop_App/Form1.vb
+++ Keats_Desktop_App/Form1.vb
@@ -1,6 +1,12 @@
-﻿Public Class Form1
+﻿Imports IBM.Data.DB2
+Public Class Form1
+    Private DbConnection As Common.DbConnection
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
     End Sub
 End Class
