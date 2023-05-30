b7cd479c61e409124b329d11fcdc14bf978ce2e5
earlsab <62688314+earlsab@users.noreply.github.com>
2023-05-30 22:04:22 +0800

    working placeholder for today's history page

--- Keats_Desktop_App/MainHomePage.Designer.vb
+++ Keats_Desktop_App/MainHomePage.Designer.vb
@@ -22,9 +22,6 @@ Partial Class MainHomePage
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
-        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
-        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
@@ -32,9 +29,11 @@ Partial Class MainHomePage
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
-        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -59,7 +58,7 @@ Partial Class MainHomePage
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(27, 225)
+        Me.DataGridView1.Location = New System.Drawing.Point(27, 241)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.Size = New System.Drawing.Size(577, 106)
         Me.DataGridView1.TabIndex = 3
@@ -77,7 +76,7 @@ Partial Class MainHomePage
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(24, 201)
+        Me.Label3.Location = New System.Drawing.Point(24, 217)
         Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(88, 13)
@@ -86,7 +85,7 @@ Partial Class MainHomePage
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(117, 195)
+        Me.Button2.Location = New System.Drawing.Point(117, 211)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(171, 24)
         Me.Button2.TabIndex = 6
@@ -102,28 +101,51 @@ Partial Class MainHomePage
         Me.Button3.Text = "Profile"
         Me.Button3.UseVisualStyleBackColor = True
         '
-        'Chart1
+        'TextBox1
         '
-        ChartArea1.Name = "ChartArea1"
-        Me.Chart1.ChartAreas.Add(ChartArea1)
-        Legend1.Name = "Legend1"
-        Me.Chart1.Legends.Add(Legend1)
-        Me.Chart1.Location = New System.Drawing.Point(27, 55)
-        Me.Chart1.Name = "Chart1"
-        Series1.ChartArea = "ChartArea1"
-        Series1.Legend = "Legend1"
-        Series1.Name = "Series1"
-        Me.Chart1.Series.Add(Series1)
-        Me.Chart1.Size = New System.Drawing.Size(300, 54)
-        Me.Chart1.TabIndex = 8
-        Me.Chart1.Text = "Chart1"
+        Me.TextBox1.Location = New System.Drawing.Point(117, 62)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(159, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(38, 65)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(74, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Meals Logged"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(38, 91)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Meals Logged"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(117, 88)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(159, 20)
+        Me.TextBox2.TabIndex = 10
         '
         'MainHomePage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(636, 359)
-        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label3)
@@ -134,7 +156,6 @@ Partial Class MainHomePage
         Me.Name = "MainHomePage"
         Me.Text = "MainHomePage"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -146,5 +167,8 @@ Partial Class MainHomePage
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Button3 As System.Windows.Forms.Button
-    Friend WithEvents Chart1 As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
 End Class
--- Keats_Desktop_App/MainHomePage.vb
+++ Keats_Desktop_App/MainHomePage.vb
@@ -1,6 +1,37 @@
-﻿Public Class MainHomePage
+﻿Imports IBM.Data.DB2
+Public Class MainHomePage
+
+    Private Sub PopulateDataGrid()
+        Dim StrStud As String
+        Dim row As String()
+        Dim CmdStud As DB2Command
+        Dim RdrStud As DB2DataReader
+        Try
+            StrStud = "Select * from intake"
+            CmdStud = New DB2Command(StrStud, Globals.DBConnLogin)
+            RdrStud = CmdStud.ExecuteReader
+            Me.DataGridView1.Rows.Clear()
+            While RdrStud.Read
+                row = New String() {RdrStud.GetString(2), RdrStud.GetString(5), _
+                                   RdrStud.GetString(4)}  ' NOT REPRESENTATIVE OF THE DATABASE. Change later
+                Me.DataGridView1.Rows.Add(row)
+            End While
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
 
     Private Sub MainHomePage_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Me.DataGridView1.ColumnCount = 4
+            Me.DataGridView1.Columns(0).Name = "Time Recorded"
+            Me.DataGridView1.Columns(1).Name = "Food"
+            Me.DataGridView1.Columns(2).Name = "Serving Size"
+            Call PopulateDataGrid()
+        Catch ex As Exception
+
+        End Try
 
     End Sub
 
@@ -17,6 +48,10 @@
     End Sub
 
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
 
     End Sub
 End Class
