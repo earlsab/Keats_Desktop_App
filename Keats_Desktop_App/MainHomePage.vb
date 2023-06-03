d3f1c7abf7c8ab75d7ed3488ed258016cab03758
earlsab <62688314+earlsab@users.noreply.github.com>
2023-06-04 04:24:22 +0800

    FINALLY FIXED GIT COMMIT ERROR
    AHHHHHH

--- Keats_Desktop_App/MainHomePage.vb
+++ Keats_Desktop_App/MainHomePage.vb
@@ -1,9 +1,54 @@
 ﻿Imports IBM.Data.DB2
-Public Class MainHomePage
 
-    Private Sub PopulateDataGrid()
+Public Class MainHomePage
+    Dim IntakeCount As Integer
+    Dim CurrentDate As Date
+
+    Private Sub CreateDailyNutrientEntry()
+        Dim StrLoad As String
+        Dim CmdLoad As DB2Command
+        Dim RdrLoad As DB2DataReader
+        Dim StrInsert As String
+        Dim CmdInsert As DB2Command
+
+        Dim LatestEntryDate As Date
+        Dim FirstEntryState As Boolean
+        ' CREATION OF DAILY NUTRIENTS ENTRY
+        FirstEntryState = False
+        Try
+            StrLoad = "select max(date_created) from daily_nutrients where account_id=" & Globals.UserAccountID
+            CmdLoad = New DB2Command(StrLoad, Globals.DBConnLogin)
+            RdrLoad = CmdLoad.ExecuteReader
+            RdrLoad.Read()
+            LatestEntryDate = RdrLoad.GetDate(0)
+            CurrentDate = DateAndTime.Today()
+
+        Catch ex As Exception
+            FirstEntryState = True
+        End Try
+
+        If DateDiff("d", LatestEntryDate, CurrentDate) > 0 Or FirstEntryState Then
+            Try
+                StrInsert = "insert into daily_nutrients(account_id, date_created, calories, protein, carbs, fats," _
+                & "max_calories, max_protein, max_carbs, max_fats, diet_plan_id, activity_lvl_id)" _
+                & "values(@AccountId, @DateTimeValue, 0, 0, 0, 0, 0, 0, 0, 0, 1, 21)"
+                CmdInsert = New DB2Command(StrInsert, Globals.DBConnLogin)
+                CmdInsert.Parameters.Add("@AccountId", IBM.Data.DB2.DB2Type.Integer).Value = Globals.UserAccountID
+                CmdInsert.Parameters.Add("@DateTimeValue", IBM.Data.DB2.DB2Type.DateTime).Value = DateTime.Now ' Assuming you want to use the current date and time
+                CmdInsert.ExecuteNonQuery()
+                MsgBox("Created New Daily Nutrient Entry")
+            Catch ex As Exception
+                MsgBox("Daily Nutrient Insert Error")
+                MsgBox(ex.ToString)
+            End Try
+        Else
+            MsgBox("Existing Entry Found. Did Not Create New Daily Nutrient Entry. ")
+        End If
+    End Sub
+    Public Sub PopulateDataGrid()
         Dim StrSum As String
         Dim row() As String
+
         Dim ingredientMappingID As Integer
         Dim ingredientNameID As Integer
         Dim ingredientKindID As Integer
@@ -19,28 +64,25 @@ Public Class MainHomePage
         Dim RdrSum2 As DB2DataReader
         Dim RdrSum3 As DB2DataReader
 
-
-
         Try
             StrSum = "Select * from intake where account_id=" & Globals.UserAccountID
             CmdSum = New DB2Command(StrSum, Globals.DBConnLogin)
             RdrSum = CmdSum.ExecuteReader
             Me.DataGridView1.Rows.Clear()
 
-
+            Me.IntakeCount = 0
             While RdrSum.Read
+                Me.IntakeCount += 1
                 ' Order of Table
                 ' 0 2ID
                 ' 1 ACCOUNT_ID()
                 ' 2 DATE_CREATED()
                 ' 3 AMOUNT()
-                ' 4 AMOUNT_UNIT()
-                ' 5 AMOUNT_UNIT_DESC()
-                ' 6 SERVING_SIZE()
-                ' 7 INGREDIENT_MAPPING_ID()
+                ' 4 SERVING_SIZE()
+                ' 5 INGREDIENT_MAPPING_ID()
 
                 ' Get details of the ingredient item.
-                ingredientMappingID = RdrSum.GetInt32(7)
+                ingredientMappingID = RdrSum.GetInt32(5)
                 Try
                     StrSum = "Select * from ingredient_mapping where id=" & ingredientMappingID
                     CmdSum = New DB2Command(StrSum, Globals.DBConnLogin)
@@ -111,6 +153,41 @@ Public Class MainHomePage
 
 
     Private Sub MainHomePage_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim StrLoad As String
+        Dim CmdLoad As DB2Command
+        Dim RdrLoad As DB2DataReader
+
+        'ID()
+        'ACCOUNT_ID()
+        'DATE_CREATED()
+        'CALORIES()
+        'PROTEIN()
+        'CARBS()
+        'FATS()
+        'MAX_CALORIES()
+        'MAX_PROTEIN()
+        'MAX_CARBS()
+        'MAX_FATS()
+        'ACTIVITY_LVL_ID()
+        'DIET_PLAN_ID()
+
+        Call CreateDailyNutrientEntry()
+ 
+        ' TABLE POPULATE
+        Try
+            StrLoad = "select calories from daily_nutrients where account_id=" & Globals.UserAccountID & _
+            " AND Date_Created ='" & CurrentDate & "'"
+            CmdLoad = New DB2Command(StrLoad, Globals.DBConnLogin)
+            RdrLoad = CmdLoad.ExecuteReader
+            RdrLoad.Read()
+            Me.CaloriesTextBox.Text = RdrLoad.GetFloat(0)
+
+        Catch ex As Exception
+            MsgBox("Error Displaying Summary")
+            MsgBox(ex.ToString)
+        End Try
+
         Try
             Me.DataGridView1.ColumnCount = 4
             Me.DataGridView1.Columns(0).Name = "Time Recorded"
@@ -118,6 +195,7 @@ Public Class MainHomePage
             Me.DataGridView1.Columns(2).Name = "Serving Size (g)"
             Me.DataGridView1.Columns(3).Name = "Calories"
             Call PopulateDataGrid()
+            Me.MealsLoggedTextBox.Text = Me.IntakeCount
         Catch ex As Exception
 
         End Try
@@ -137,19 +215,32 @@ Public Class MainHomePage
     End Sub
 
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
-
+        EditSpecific.Show()
     End Sub
 
     Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
 
     End Sub
 
-    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles AddMealButton.Click
         SearchIngredient.Show()
         Me.Hide()
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
 
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles CaloriesTextBox.TextChanged
+
+    End Sub
+
+    Private Sub MealsLoggedTextBox_TextChanged(sender As Object, e As EventArgs) Handles MealsLoggedTextBox.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ViewAll.Show()
+        Me.Hide()
     End Sub
 End Class
