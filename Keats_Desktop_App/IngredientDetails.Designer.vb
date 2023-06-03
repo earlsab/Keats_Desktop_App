70058544654c7dd83a3e3f14912d0bb5d900a6c6
Cloyd Abad <abadcloyd@gmail.com>
2023-06-03 11:37:02 +0800

    Querying subvariant and nutrients on initial load of ingredient details

--- Keats_Desktop_App/IngredientDetails.Designer.vb
+++ Keats_Desktop_App/IngredientDetails.Designer.vb
@@ -33,10 +33,16 @@ Partial Class IngredientDetails
         Me.SelectedSubvariant = New System.Windows.Forms.Label()
         Me.SelectedSubvariantValue = New System.Windows.Forms.Label()
         Me.SelectedVariantValue = New System.Windows.Forms.Label()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SelectedIngredientValue = New System.Windows.Forms.Label()
         Me.SelectedIngredient = New System.Windows.Forms.Label()
         Me.Calories = New System.Windows.Forms.Label()
         Me.CaloriesValue = New System.Windows.Forms.Label()
+        Me.ProteinValue = New System.Windows.Forms.Label()
+        Me.Protein = New System.Windows.Forms.Label()
+        Me.CarbsValue = New System.Windows.Forms.Label()
+        Me.Carbs = New System.Windows.Forms.Label()
+        Me.FatsValue = New System.Windows.Forms.Label()
+        Me.Fats = New System.Windows.Forms.Label()
         CType(Me.IngredientVariant, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.IngredientSubvariant, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -52,7 +58,7 @@ Partial Class IngredientDetails
         Me.IngredientVariant.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None
         Me.IngredientVariant.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.IngredientVariant.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IngredientID, Me.IngredientName})
-        Me.IngredientVariant.Location = New System.Drawing.Point(12, 105)
+        Me.IngredientVariant.Location = New System.Drawing.Point(13, 53)
         Me.IngredientVariant.MultiSelect = False
         Me.IngredientVariant.Name = "IngredientVariant"
         Me.IngredientVariant.ReadOnly = True
@@ -60,7 +66,7 @@ Partial Class IngredientDetails
         Me.IngredientVariant.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
         Me.IngredientVariant.ShowCellToolTips = False
         Me.IngredientVariant.ShowEditingIcon = False
-        Me.IngredientVariant.Size = New System.Drawing.Size(500, 267)
+        Me.IngredientVariant.Size = New System.Drawing.Size(500, 177)
         Me.IngredientVariant.TabIndex = 4
         '
         'IngredientID
@@ -90,7 +96,7 @@ Partial Class IngredientDetails
         Me.IngredientSubvariant.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None
         Me.IngredientSubvariant.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.IngredientSubvariant.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2})
-        Me.IngredientSubvariant.Location = New System.Drawing.Point(518, 105)
+        Me.IngredientSubvariant.Location = New System.Drawing.Point(519, 53)
         Me.IngredientSubvariant.MultiSelect = False
         Me.IngredientSubvariant.Name = "IngredientSubvariant"
         Me.IngredientSubvariant.ReadOnly = True
@@ -98,7 +104,7 @@ Partial Class IngredientDetails
         Me.IngredientSubvariant.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
         Me.IngredientSubvariant.ShowCellToolTips = False
         Me.IngredientSubvariant.ShowEditingIcon = False
-        Me.IngredientSubvariant.Size = New System.Drawing.Size(500, 267)
+        Me.IngredientSubvariant.Size = New System.Drawing.Size(500, 177)
         Me.IngredientSubvariant.TabIndex = 5
         '
         'DataGridViewTextBoxColumn1
@@ -120,7 +126,7 @@ Partial Class IngredientDetails
         'SelectedVariant
         '
         Me.SelectedVariant.AutoSize = True
-        Me.SelectedVariant.Location = New System.Drawing.Point(12, 86)
+        Me.SelectedVariant.Location = New System.Drawing.Point(13, 34)
         Me.SelectedVariant.Name = "SelectedVariant"
         Me.SelectedVariant.Size = New System.Drawing.Size(103, 13)
         Me.SelectedVariant.TabIndex = 6
@@ -129,7 +135,7 @@ Partial Class IngredientDetails
         'SelectedSubvariant
         '
         Me.SelectedSubvariant.AutoSize = True
-        Me.SelectedSubvariant.Location = New System.Drawing.Point(515, 86)
+        Me.SelectedSubvariant.Location = New System.Drawing.Point(516, 34)
         Me.SelectedSubvariant.Name = "SelectedSubvariant"
         Me.SelectedSubvariant.Size = New System.Drawing.Size(148, 13)
         Me.SelectedSubvariant.TabIndex = 8
@@ -138,7 +144,7 @@ Partial Class IngredientDetails
         'SelectedSubvariantValue
         '
         Me.SelectedSubvariantValue.AutoSize = True
-        Me.SelectedSubvariantValue.Location = New System.Drawing.Point(669, 86)
+        Me.SelectedSubvariantValue.Location = New System.Drawing.Point(670, 34)
         Me.SelectedSubvariantValue.Name = "SelectedSubvariantValue"
         Me.SelectedSubvariantValue.Size = New System.Drawing.Size(10, 13)
         Me.SelectedSubvariantValue.TabIndex = 9
@@ -147,20 +153,20 @@ Partial Class IngredientDetails
         'SelectedVariantValue
         '
         Me.SelectedVariantValue.AutoSize = True
-        Me.SelectedVariantValue.Location = New System.Drawing.Point(121, 86)
+        Me.SelectedVariantValue.Location = New System.Drawing.Point(122, 34)
         Me.SelectedVariantValue.Name = "SelectedVariantValue"
         Me.SelectedVariantValue.Size = New System.Drawing.Size(10, 13)
         Me.SelectedVariantValue.TabIndex = 10
         Me.SelectedVariantValue.Text = "."
         '
-        'Label1
+        'SelectedIngredientValue
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(121, 9)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(10, 13)
-        Me.Label1.TabIndex = 12
-        Me.Label1.Text = "."
+        Me.SelectedIngredientValue.AutoSize = True
+        Me.SelectedIngredientValue.Location = New System.Drawing.Point(121, 9)
+        Me.SelectedIngredientValue.Name = "SelectedIngredientValue"
+        Me.SelectedIngredientValue.Size = New System.Drawing.Size(10, 13)
+        Me.SelectedIngredientValue.TabIndex = 12
+        Me.SelectedIngredientValue.Text = "."
         '
         'SelectedIngredient
         '
@@ -174,29 +180,89 @@ Partial Class IngredientDetails
         'Calories
         '
         Me.Calories.AutoSize = True
-        Me.Calories.Location = New System.Drawing.Point(9, 411)
+        Me.Calories.Location = New System.Drawing.Point(10, 247)
         Me.Calories.Name = "Calories"
-        Me.Calories.Size = New System.Drawing.Size(47, 13)
+        Me.Calories.Size = New System.Drawing.Size(78, 13)
         Me.Calories.TabIndex = 13
-        Me.Calories.Text = "Calories:"
+        Me.Calories.Text = "Calories (KCal):"
         '
         'CaloriesValue
         '
         Me.CaloriesValue.AutoSize = True
-        Me.CaloriesValue.Location = New System.Drawing.Point(62, 411)
+        Me.CaloriesValue.Location = New System.Drawing.Point(94, 247)
         Me.CaloriesValue.Name = "CaloriesValue"
         Me.CaloriesValue.Size = New System.Drawing.Size(10, 13)
         Me.CaloriesValue.TabIndex = 14
         Me.CaloriesValue.Text = "."
         '
+        'ProteinValue
+        '
+        Me.ProteinValue.AutoSize = True
+        Me.ProteinValue.Location = New System.Drawing.Point(223, 247)
+        Me.ProteinValue.Name = "ProteinValue"
+        Me.ProteinValue.Size = New System.Drawing.Size(10, 13)
+        Me.ProteinValue.TabIndex = 16
+        Me.ProteinValue.Text = "."
+        '
+        'Protein
+        '
+        Me.Protein.AutoSize = True
+        Me.Protein.Location = New System.Drawing.Point(159, 247)
+        Me.Protein.Name = "Protein"
+        Me.Protein.Size = New System.Drawing.Size(58, 13)
+        Me.Protein.TabIndex = 15
+        Me.Protein.Text = "Protein (g):"
+        '
+        'CarbsValue
+        '
+        Me.CarbsValue.AutoSize = True
+        Me.CarbsValue.Location = New System.Drawing.Point(362, 247)
+        Me.CarbsValue.Name = "CarbsValue"
+        Me.CarbsValue.Size = New System.Drawing.Size(10, 13)
+        Me.CarbsValue.TabIndex = 18
+        Me.CarbsValue.Text = "."
+        '
+        'Carbs
+        '
+        Me.Carbs.AutoSize = True
+        Me.Carbs.Location = New System.Drawing.Point(304, 247)
+        Me.Carbs.Name = "Carbs"
+        Me.Carbs.Size = New System.Drawing.Size(52, 13)
+        Me.Carbs.TabIndex = 17
+        Me.Carbs.Text = "Carbs (g):"
+        '
+        'FatsValue
+        '
+        Me.FatsValue.AutoSize = True
+        Me.FatsValue.Location = New System.Drawing.Point(503, 247)
+        Me.FatsValue.Name = "FatsValue"
+        Me.FatsValue.Size = New System.Drawing.Size(10, 13)
+        Me.FatsValue.TabIndex = 20
+        Me.FatsValue.Text = "."
+        '
+        'Fats
+        '
+        Me.Fats.AutoSize = True
+        Me.Fats.Location = New System.Drawing.Point(452, 247)
+        Me.Fats.Name = "Fats"
+        Me.Fats.Size = New System.Drawing.Size(45, 13)
+        Me.Fats.TabIndex = 19
+        Me.Fats.Text = "Fats (g):"
+        '
         'IngredientDetails
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1031, 788)
+        Me.ClientSize = New System.Drawing.Size(1031, 427)
+        Me.Controls.Add(Me.FatsValue)
+        Me.Controls.Add(Me.Fats)
+        Me.Controls.Add(Me.CarbsValue)
+        Me.Controls.Add(Me.Carbs)
+        Me.Controls.Add(Me.ProteinValue)
+        Me.Controls.Add(Me.Protein)
         Me.Controls.Add(Me.CaloriesValue)
         Me.Controls.Add(Me.Calories)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SelectedIngredientValue)
         Me.Controls.Add(Me.SelectedIngredient)
         Me.Controls.Add(Me.SelectedVariantValue)
         Me.Controls.Add(Me.SelectedSubvariantValue)
@@ -215,7 +281,7 @@ Partial Class IngredientDetails
     End Sub
     Friend WithEvents IngredientVariant As System.Windows.Forms.DataGridView
     Friend WithEvents IngredientSubvariant As System.Windows.Forms.DataGridView
-    Friend WithEvents SelectedVariant As System.Windows.Forms.Label 
+    Friend WithEvents SelectedVariant As System.Windows.Forms.Label
     Friend WithEvents IngredientID As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents IngredientName As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents DataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
@@ -223,8 +289,14 @@ Partial Class IngredientDetails
     Friend WithEvents SelectedSubvariant As System.Windows.Forms.Label
     Friend WithEvents SelectedSubvariantValue As System.Windows.Forms.Label
     Friend WithEvents SelectedVariantValue As System.Windows.Forms.Label
-    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents SelectedIngredientValue As System.Windows.Forms.Label
     Friend WithEvents SelectedIngredient As System.Windows.Forms.Label
     Friend WithEvents Calories As System.Windows.Forms.Label
     Friend WithEvents CaloriesValue As System.Windows.Forms.Label
+    Friend WithEvents ProteinValue As System.Windows.Forms.Label
+    Friend WithEvents Protein As System.Windows.Forms.Label
+    Friend WithEvents CarbsValue As System.Windows.Forms.Label
+    Friend WithEvents Carbs As System.Windows.Forms.Label
+    Friend WithEvents FatsValue As System.Windows.Forms.Label
+    Friend WithEvents Fats As System.Windows.Forms.Label
 End Class
--- Keats_Desktop_App/IngredientDetails.vb
+++ Keats_Desktop_App/IngredientDetails.vb
@@ -2,9 +2,8 @@
 Imports System.Collections.Generic
 
 Public Class IngredientDetails
-     
-    Dim IngredientVariants As New Dictionary(Of Integer, String)()
-    Dim IngredientSubvariants As New Dictionary(Of Integer, String)()
+    Dim FirstIngredientVariantId As Integer
+    Dim FirstIngredientSubvariantId As Integer
 
     Private Sub DomainUpDown1_SelectedItemChanged(sender As Object, e As EventArgs)
 
@@ -12,7 +11,7 @@ Public Class IngredientDetails
 
 
 
-    Private Sub PopulateIngredientMap()
+    Private Sub PopulateIngredientVariant()
         Dim StrStud As String
         Dim row As String()
         Dim CmdStud As DB2Command
@@ -28,14 +27,12 @@ Public Class IngredientDetails
             CmdStud = New DB2Command(StrStud, Globals.DBConnLogin)
             RdrStud = CmdStud.ExecuteReader
             IngredientVariant.Rows.Clear()
+            Dim variantCount = 0
             While RdrStud.Read
-                Dim currKey As Integer = RdrStud.GetInt32(0)
-                Dim currValue As String = RdrStud.GetString(1)
-                If Not IngredientVariants.ContainsKey(currKey) Then
-                    IngredientVariants.Add(currKey, currValue)
-                    Console.WriteLine("Item added successfully.")
-                Else
-                    Console.WriteLine("Key already exists. Item not added.")
+                If variantCount < 1 Then
+                    FirstIngredientVariantId = RdrStud.GetInt32(0)
+                    SelectedVariantValue.Text() = RdrStud.GetString(1)
+                    variantCount = 1
                 End If
                 row = New String() {RdrStud.GetString(0), RdrStud.GetString(1)}
                 IngredientVariant.Rows.Add(row)
@@ -45,13 +42,84 @@ Public Class IngredientDetails
 
         End Try
     End Sub
+    Private Sub PopulateIngredientSubvariant()
+        Dim StrStud As String
+        Dim row As String()
+        Dim CmdStud As DB2Command
+        Dim RdrStud As DB2DataReader
+        Try
+            StrStud = "select ingredient_subvariant.id, ingredient_subvariant.name " _
+                & "FROM ingredient_mapping " _
+                & "JOIN ingredient_subvariant ON ingredient_mapping.ingredient_subvariant_id = ingredient_subvariant.id " _
+                & "WHERE ingredient_mapping.ingredient_id = " & Globals.SelectedIngredientId & " " _
+                & "AND ingredient_mapping.ingredient_variant_id = " & FirstIngredientVariantId & ";"
+            CmdStud = New DB2Command(StrStud, Globals.DBConnLogin)
+            RdrStud = CmdStud.ExecuteReader
+            IngredientSubvariant.Rows.Clear()
+            Dim subvariantCount = 0
+            While RdrStud.Read
+                If subvariantCount < 1 Then
+                    FirstIngredientSubvariantId = RdrStud.GetInt32(0)
+                    SelectedSubvariantValue.Text() = RdrStud.GetString(1)
+                    subvariantCount = 1
+                End If
+                row = New String() {RdrStud.GetString(0), RdrStud.GetString(1)}
+                IngredientSubvariant.Rows.Add(row)
+            End While
+        Catch ex As Exception
+            MsgBox(ex.ToString)
 
+        End Try
+    End Sub
+    Private Sub RetrieveIngredient()
+        Dim StrStud As String
+        Dim CmdStud As DB2Command
+        Dim RdrStud As DB2DataReader
+        Try
+            StrStud = "select name FROM ingredient WHERE id = " & Globals.SelectedIngredientId
+            CmdStud = New DB2Command(StrStud, Globals.DBConnLogin)
+            RdrStud = CmdStud.ExecuteReader
+            While RdrStud.Read
+                SelectedIngredientValue.Text() = RdrStud.GetString(0)
+            End While
+        Catch ex As Exception
+            MsgBox(ex.ToString)
 
+        End Try
+    End Sub
+    Private Sub PopulateNutrients()
+        Dim StrStud As String 
+        Dim CmdStud As DB2Command
+        Dim RdrStud As DB2DataReader
+        Try
+            StrStud = "select nutrient.calories, nutrient.protein, nutrient.carbs, nutrient.fats " _
+                & "FROM ingredient_mapping " _
+                & "JOIN nutrient ON ingredient_mapping.nutrient_id = nutrient.id " _
+                & "WHERE ingredient_mapping.ingredient_id = " & Globals.SelectedIngredientId & " " _
+                & "AND ingredient_mapping.ingredient_variant_id = " & FirstIngredientVariantId & " " _
+                & "AND ingredient_mapping.ingredient_subvariant_id = " & FirstIngredientSubvariantId & ";"
+            CmdStud = New DB2Command(StrStud, Globals.DBConnLogin)
+            RdrStud = CmdStud.ExecuteReader 
+            While RdrStud.Read
+                CaloriesValue.Text() = RdrStud.GetString(0)
+                ProteinValue.Text() = RdrStud.GetString(1)
+                CarbsValue.Text() = RdrStud.GetString(2)
+                FatsValue.Text() = RdrStud.GetString(3)
+            End While
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+
+        End Try
+    End Sub
     Private Sub IngredientDetails_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim ingId As Integer
         ingId = Globals.SelectedIngredientId
-        If ingId > 0 Then
-            Call PopulateIngredientMap()
+        If Not ingId = 0 Then
+            Call PopulateIngredientVariant()
+            Call RetrieveIngredient()
+            Call PopulateIngredientSubvariant()
+            Call PopulateNutrients()
         End If
     End Sub
+     
 End Class
