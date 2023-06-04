16ef9cb538279fd8dd193f7b63484be2e9ad6db7
earlsab <62688314+earlsab@users.noreply.github.com>
2023-06-04 12:49:39 +0800

    remove data dump

--- Keats_Desktop_App/data dump/activity_lvl.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO activity_lvl (id,name,main_image_link,background_color,activity_lvl_desc,bmr_multiplier) VALUES
-	 ('a2d1c54d-e27b-4810-8ed0-12c061d42567','inactive',NULL,NULL,'This is characterized by low levels of physical activity and a lot of sitting or lying down throughout the day.',1.2),
-	 ('d7b88b02-72fe-49c1-b912-398522db980f','lightly active',NULL,NULL,'Increased heart rate and breathing for at least about 15-30 minutes, 1-3 days a week.',1.375),
-	 ('6e2891c4-d47c-4635-8ceb-2d838ca72fb8','moderately active',NULL,NULL,'Increased heart rate and breathing for at least about 15-30 minutes, 4-5 days a week.',1.465),
-	 ('52c8a812-ec3e-44db-843b-bb59bb976458','active',NULL,NULL,'Increased heart rate and breathing for at least about 45-120 minutes, 3-4 days a week.',1.55),
-	 ('25a358f6-656b-4b17-81fb-8d2f1e832a78','very active',NULL,NULL,'Increased heart rate and breathing for at least about 2 hours or more, 6-7 days a week.',1.725),
-	 ('669520e4-0ce6-452c-8960-97d4920b7bcf','extremely active',NULL,NULL,'Increased heart rate and breathing for at least about 2 hours or more, everyday.',1.9);
