a936c76f946b9a8edb81882edb5e5c54d33dc600
earlsab <62688314+earlsab@users.noreply.github.com>
2023-06-05 23:37:16 +0800

    added all the new forms (not working)

---
+++ Keats_Desktop_App/IngredientAdminEdit.vb
@@ -0,0 +1,3 @@
+﻿Public Class IngredientAdminEdit
+
+End Class
--- Keats_Desktop_App/MainHomePage.vb
+++ Keats_Desktop_App/MainHomePage.vb
@@ -303,13 +303,23 @@ Public Class MainHomePage
 
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs)
-        ViewIntakesAll.Show()
-        Me.Hide()
-    End Sub
-     
     Private Sub ProfileManager_Click(sender As Object, e As EventArgs) Handles ProfileManager.Click
         UserProfile.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles Button1.Click
+        IngredientAdminEdit.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        ViewAllSummaries.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ViewIntakesAll.Show()
         Me.Hide()
     End Sub
 End Class
