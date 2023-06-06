97ad6b407ea3f4ebfe16a6a11ce348e7a657ed89
patrickpersonguy <patricklpatrimonio@su.edu.ph>
2023-06-06 10:01:05 +0800

    committing EDIT and DELETE
    THEY WROK ALHJKHSJKHJKLBKHKQW

--- Keats_Desktop_App/UserProfile.vb
+++ Keats_Desktop_App/UserProfile.vb
@@ -1,8 +1,76 @@
 ﻿Imports IBM.Data.DB2
 Public Class UserProfile
 
-    Private Sub EditUserDetails()
+    Private Sub EditUserDetails_Consumer()
+        Dim StrEdit As String
+        Dim CmdEdit As DB2Command
 
+        StrEdit = "Update consumer_profile set name_first = '" & TextBox2.Text & "', " _
+            & "name_last = '" & TextBox1.Text & "', " _
+            & "phone_number = '" & TextBoxPh.Text & "', " _
+            & "date_updated = current_timestamp " _
+            & "where account_id = " & Globals.UserAccountID
+
+        CmdEdit = New DB2Command(StrEdit, Globals.DBConnLogin)
+        CmdEdit.ExecuteNonQuery()
+    End Sub
+
+    Private Sub EditUserDetails_Account()
+        Dim DPid As Integer
+        Dim StrDP As String
+        Dim CmdDP As DB2Command
+        Dim RdrDP As DB2DataReader
+
+        Dim ALid As Integer
+        Dim StrAL As String
+        Dim CmdAL As DB2Command
+        Dim RdrAL As DB2DataReader
+
+        Dim weight = Integer.Parse(TextBoxW.Text)
+        Dim Height = Integer.Parse(TextBoxH.Text)
+
+        Dim StrEdit As String
+        Dim CmdEdit As DB2Command
+
+        StrAL = "Select id FROM activity_lvl where name = '" & ComboBox1.Text & "'"
+        CmdAL = New DB2Command(StrAL, Globals.DBConnLogin)
+        RdrAL = CmdAL.ExecuteReader
+
+        RdrAL.Read()
+        ALid = RdrAL.GetInt32(0)
+
+        RdrAL.Close()
+
+        StrDP = "Select id FROM diet_plan where name = '" & ComboBox2.Text & "'"
+        CmdDP = New DB2Command(StrDP, Globals.DBConnLogin)
+        RdrDP = CmdDP.ExecuteReader
+
+        RdrDP.Read()
+        DPid = RdrDP.GetInt32(0)
+
+        RdrDP.Close()
+
+        StrEdit = "Update account_vitals set birthday = '" & DateTimePicker1.Value & "', " _
+            & "weight = " & weight & ", " _
+            & "height = " & Height & ", " _
+            & "activity_lvl_id = " & ALid & ", " _
+            & "diet_plan_id = " & DPid & " " _
+            & "where account_id = " & Globals.UserAccountID
+
+        CmdEdit = New DB2Command(StrEdit, Globals.DBConnLogin)
+        CmdEdit.ExecuteNonQuery()
+    End Sub
+
+    Private Sub RefreshTextBoxes()
+        TextBox2.Refresh()
+        TextBox1.Refresh()
+        DateTimePicker1.Refresh()
+        TextBoxPh.Refresh()
+        TextBoxSex.Refresh()
+        TextBoxW.Refresh()
+        TextBoxH.Refresh()
+        ComboBox1.Refresh()
+        ComboBox2.Refresh()
     End Sub
 
     Private Sub EnableTextBoxes()
@@ -30,6 +98,23 @@ Public Class UserProfile
     End Sub
 
     Private Sub DeleteUserProfile()
+        Dim StrDelete As String
+        Dim CmdDelete As DB2Command
+        Dim CheckDelete As Integer
+
+        StrDelete = "Delete from consumer_profile where account_id = " & Globals.UserAccountID & "; " &
+            "delete from intake where account_id = " & Globals.UserAccountID & "; " &
+            "delete from daily_nutrients where account_id = " & Globals.UserAccountID & "; " &
+            "delete from account_vitals where account_id = " & Globals.UserAccountID & "; " &
+            "delete from account where id = " & Globals.UserAccountID
+        CmdDelete = New DB2Command(StrDelete, Globals.DBConnLogin)
+        CheckDelete = CmdDelete.ExecuteNonQuery()
+
+        If CheckDelete > 0 Then
+            MessageBox.Show("Delete was successful. Rows of data deleted: " & CheckDelete)
+        Else
+            MessageBox.Show("Delete not completed...")
+        End If
 
     End Sub
 
@@ -125,6 +210,7 @@ Public Class UserProfile
     End Sub
 
     Private Sub UserProfile_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call RefreshTextBoxes()
         Call LoadAccount()
         Call LoadConsumer_Profile()
         Call LoadAccount_vitals()
@@ -133,7 +219,6 @@ Public Class UserProfile
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Edit.Click
 
         If MsgBox("Are you sure you want to EDIT your profile details?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
-            Call EditUserDetails()
 
             Me.Edit.Enabled = False
             Me.SaveEdit.Enabled = True
@@ -147,7 +232,20 @@ Public Class UserProfile
     End Sub
 
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles SaveEdit.Click
+        If MsgBox("Are you sure you want to CONFIRM EDITTED DETAILS of your profile?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+            Call EditUserDetails_Consumer()
+            Call EditUserDetails_Account()
 
+            Me.Edit.Enabled = True
+            Me.SaveEdit.Enabled = False
+            Me.Cancel.Enabled = False
+            Me.Delete.Enabled = True
+            Me.Back.Enabled = True
+            Call DisableTextBoxes()
+            Call RefreshTextBoxes()
+
+            MessageBox.Show("New details now saved!")
+        End If
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Back.Click
@@ -159,7 +257,6 @@ Public Class UserProfile
 
     Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Cancel.Click
         If MsgBox("Are you sure you want to CANCEL EDITTING your profile details?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
-            Call EditUserDetails()
 
             Me.Edit.Enabled = True
             Me.SaveEdit.Enabled = False
@@ -173,8 +270,11 @@ Public Class UserProfile
 
     Private Sub Button2_Click_2(sender As Object, e As EventArgs) Handles Delete.Click
 
-        If MsgBox("Are you sure you want to DELETE profile?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+        If MsgBox("WARNING: Are you sure you want to DELETE profile?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
             Call DeleteUserProfile()
+            Me.Close()
+            LoginForm1.Show()
+
         End If
 
     End Sub
